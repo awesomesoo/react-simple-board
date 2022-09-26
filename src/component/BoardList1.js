1deaeb224cd825e443e553e7a9b974ae4d087dbe
uiuxsooa <sooa0513@naver.com>
2022-09-27 02:14:16 +0900

    리스트 내용 선택적으로 보여주기(번호, 작성자)

--- src/component/BoardList1.js
+++ src/component/BoardList1.js
@@ -13,14 +13,27 @@ const dataList = [
     brdtitle: '나만 없어 강아지ㅠㅠ',
     brddate: new Date(),
   },
+  {
+    brdno: 3,
+    brdwriter: '한문철',
+    brdtitle: '급하다고 빠르게 하면 안됩니다!',
+    brddate: new Date(),
+  },
 ];
 
 const BoardList1 = () => {
-  const [data, setData] = useState(dataList)
-  const list = dataList.map(function (row) {
-    return row.brdno + row.brdwriter;
-  });
-  return <>화면 테스트{list}</>;
+  const [data, setData] = useState(dataList);
+  // const list = dataList.map(function (row) {
+  //   return row.brdno + row.brdwriter;
+  // });
+  const list = dataList.map((row) => row.brdno + row.brdwriter);
+  return (
+    <>
+      첫 번째: 리스트 내용 선택적으로 보여주기 (번호, 작성자)
+      <hr />
+      {list}
+    </>
+  );
 };
 
 export default BoardList1;
