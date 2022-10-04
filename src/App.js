9a636bb2e5f3e8eb6b134e89e70385503d9bb1e3
uxuisooa <sooa0513@naver.com>
2022-10-05 00:23:20 +0900

    게시글 등록하기 구현 작업중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import TestBoards from "./component/BoardList3";
+import TestBoards from "./component/BoardList4";
 import "./asset/style.css";
 
 function App() {
--- src/component/BoardList4.js
+++ src/component/BoardList4.js
@@ -1,7 +1,111 @@
-import React from "react";
+import React, { Component } from "react";
+/*
+    add a board (post)
+*/
+class App4 extends Component {
+  state = {
+    maxNo: 4,
+    boards: [
+      {
+        brdno: 1,
+        brdwriter: "오은영",
+        brdtitle: "React 게시판에 첫 글 등록하기~",
+        brddate: new Date(),
+      },
+      {
+        brdno: 2,
+        brdwriter: "강형욱",
+        brdtitle: "나만 없어 강아지ㅠㅠ",
+        brddate: new Date(),
+      },
+      {
+        brdno: 3,
+        brdwriter: "한문철",
+        brdtitle: "급하다고 빠르게 달리면 위험해요!",
+        brddate: new Date(),
+      },
+    ],
+  };
 
-const BoardList4 = () => {
-  return <div></div>;
-};
+  handleSaveData = (data) => {
+    this.setState({
+      boards: this.state.boards.concat({
+        brdno: this.state.maxNo++,
+        brddate: new Date(),
+        ...data,
+      }),
+    });
+  };
 
-export default BoardList4;
+  render() {
+    const { boards } = this.state;
+
+    return (
+      <div>
+        <BoardForm onSaveData={this.handleSaveData} />
+        <table border="1">
+          <tbody>
+            <tr align="center">
+              <td width="50">No.</td>
+              <td width="300">Title</td>
+              <td width="100">Name</td>
+              <td width="100">Date</td>
+            </tr>
+            {boards.map(function (row) {
+              return <BoardItem key={row.brdno} row={row} />;
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class BoardItem extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.row.brdno}</td>
+        <td>{this.props.row.brdtitle}</td>
+        <td>{this.props.row.brdwriter}</td>
+        <td>{this.props.row.brddate.toLocaleDateString("ko-KR")}</td>
+      </tr>
+    );
+  }
+}
+
+class BoardForm extends Component {
+  state = {};
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSaveData(this.state);
+    this.setState({});
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="title"
+          name="brdtitle"
+          onChange={this.handleChange}
+        />
+        <input
+          placeholder="name"
+          name="brdwriter"
+          onChange={this.handleChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+    );
+  }
+}
+
+export default App4;
